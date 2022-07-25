53e4a6d14e013c7ebd2d465e7aa7b255aba70964
Arjun Parameswaran <40994090+arjunpunnappurath@users.noreply.github.com>
2022-07-25 23:04:12 +0530

    InfiniteLoop

--- main.go
+++ main.go
@@ -22,26 +22,28 @@ func main() {
 	fmt.Printf("We have a total of %v and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here...")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	//ask user for the name
-	fmt.Println("Enter the FirstName")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter the last name")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter the email address")
-	fmt.Scan(&email)
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		//ask user for the name
+		fmt.Println("Enter the FirstName")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter the last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter the email address")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v%v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("Thank you %v%v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("These are all the bookings %v\n", bookings)
+		fmt.Printf("These are all the bookings %v\n", bookings)
 
-	fmt.Printf("%v tickets are remaining for %v", remainingTickets, conferenceName)
+		fmt.Printf("%v tickets are remaining for %v", remainingTickets, conferenceName)
+	}
 }
